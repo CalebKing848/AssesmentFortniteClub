71c99f776d1687038463c272ce143d761330d450
CalebKing848 <calebking848@outlook.com>
2023-11-15 13:47:28 +1300

    Add news functionality

---
+++ FortniteEmpire/FortniteEmpire/Models/NewsDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FortniteEmpire.Models
+{
+    public class NewsDataContext : DbContext
+    {
+        public DbSet<NewsPost> Posts { get; set; }
+
+        public NewsDataContext(DbContextOptions<NewsDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- FortniteEmpire/FortniteEmpire/Startup.cs
+++ FortniteEmpire/FortniteEmpire/Startup.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using FortniteEmpire.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,10 +14,24 @@ namespace FortniteEmpire
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<NewsDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("NewsDataContext");
+                options.UseSqlServer(connectionString);
+            });
+
             services.AddMvc();
         }
 
